a8071b20705ee0d4caf29c9329a576d95f0993de
xobiomesh <gr33nturnip@gmail.com>
2024-05-28 02:27:03 +0200

    slash commands added

---
+++ registerCommands.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const { REST, Routes } = require('@discordjs/rest');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const commands = [
+    new SlashCommandBuilder()
+        .setName('help')
+        .setDescription('Displays this help message'),
+    new SlashCommandBuilder()
+        .setName('fetch')
+        .setDescription('Fetches all messages from the channel and saves them to a file')
+].map(command => command.toJSON());
+
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_BOT_TOKEN);
+
+(async () => {
+    try {
+        console.log('Started refreshing application (/) commands.');
+
+        await rest.put(
+            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+            { body: commands }
+        );
+
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
+    }
+})();
